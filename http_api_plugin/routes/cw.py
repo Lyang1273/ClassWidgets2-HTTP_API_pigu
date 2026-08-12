41280bb3b5650733f366bbd6fd1079254d78f9d7
Lyang1273 <2609350581@qq.com>
2026-08-12 22:27:11 +0800

    add: cw信息api

--- http_api_plugin/routes/cw.py
+++ http_api_plugin/routes/cw.py
@@ -4,7 +4,7 @@ from . import register
 
 def handle_cw(ctx, query, body):
     logger.success("GET /cw 请求成功")
-    config = open("/configs/config.json", "r", encoding="utf-8")
+    config = open("/configs/configs.json", "r", encoding="utf-8")
     return 200, {'status': 'success', 'message': config}
 
 
